beab77a40d0852674c5d15629bd56338e78dbba3
DaiTou567 <vh95vvhh@gmail.com>
2022-06-16 21:38:51 -0700

    dum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware_Class.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware_Class.java
@@ -1,6 +1,14 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Hardware_Class {
+    public DCMotorEx leftfront = null;
+
+
     public Hardware_Class(){
 
     }
